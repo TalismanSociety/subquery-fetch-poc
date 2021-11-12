d706e99b316d77290570d7c6677f586c3151f495
alecdwm <alec@owls.io>
2021-11-12 13:24:02 +1000

    added subql fetch proof of concept

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,33 +1,45 @@
-import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
-import {StarterEntity} from "../types";
-import {Balance} from "@polkadot/types/interfaces";
+import { SubstrateBlock } from "@subql/types";
+import https from "https";
+import { ChainManifest } from "../types";
 
+// A period of 1800 blocks at a rate of 6 blocks per second
+// 1800 / 6 = 300 seconds = execute once every 5 minutes
+const execPeriod = 1800;
 
 export async function handleBlock(block: SubstrateBlock): Promise<void> {
-    //Create a new starterEntity with ID using block hash
-    let record = new StarterEntity(block.block.header.hash.toString());
-    //Record block number
-    record.field1 = block.block.header.number.toNumber();
-    await record.save();
+  const blockNumber = block.block.header.number.toNumber();
+
+  // skip any block which isn't a multiple of execPeriod
+  if (blockNumber % execPeriod !== 0) return;
+
+  const id = "0";
+  const result = await new Promise<string>((resolve, reject) =>
+    https
+      .get(
+        `https://raw.githubusercontent.com/TalismanSociety/chaindata/multi-relay-chain-future/${id}/manifest.json`,
+        (res) => {
+          let data = "";
+          res.on("data", (chunk) => {
+            data += chunk;
+          });
+          res.on("end", () => resolve(data));
+        }
+      )
+      .on("error", (error) => reject(error))
+  );
+
+  const manifest = JSON.parse(result);
+
+  const chainManifest =
+    (await ChainManifest.get(id)) || ChainManifest.create({ id });
+
+  chainManifest.name = manifest.name;
+  chainManifest.description = manifest.description;
+  chainManifest.nativeToken = manifest.nativeToken;
+  chainManifest.tokenDecimals = manifest.tokenDecimals;
+  chainManifest.isRelay = manifest.isRelay;
+  chainManifest.rpcs = manifest.rpcs;
+
+  // save
+  await chainManifest.save();
 }
-
-export async function handleEvent(event: SubstrateEvent): Promise<void> {
-    const {event: {data: [account, balance]}} = event;
-    //Retrieve the record by its ID
-    const record = await StarterEntity.get(event.extrinsic.block.block.header.hash.toString());
-    record.field2 = account.toString();
-    //Big integer type Balance of a transfer event
-    record.field3 = (balance as Balance).toBigInt();
-    await record.save();
-}
-
-export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-    const record = await StarterEntity.get(extrinsic.block.block.header.hash.toString());
-    //Date type timestamp
-    record.field4 = extrinsic.block.timestamp;
-    //Boolean tyep
-    record.field5 = true;
-    await record.save();
-}
-
-
